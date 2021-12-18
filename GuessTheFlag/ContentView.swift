0d5c1cf4ba64779bf9a9eb68fa95ae546f161044
Frederick Javalera <frederickjavalera@gmail.com>
2021-12-18 12:52:23 -0700

    - Adding image assets. - Adding end game summary and restarts game. - Add score tracking.

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,51 +8,98 @@
 import SwiftUI
 
 struct ContentView: View {
-  @State private var showingAlert = false
+  @State private var showingScore = false
+  @State private var scoreTitle = ""
+  @State private var score = 0
+  @State private var showAlert = false
+  @State private var attempts = 0
+  let totalQuestions = 7
+  @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
+  @State private var correctAnswer = Int.random(in: 0...2)
   var body: some View {
     ZStack {
-      
-      LinearGradient(colors: [.red, .orange], startPoint: .top, endPoint: .bottomTrailing)
+      RadialGradient(stops: [
+        .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+        .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3),
+      ], center: .top, startRadius: 200, endRadius: 400)
+        .ignoresSafeArea()
       
       VStack {
-        Spacer()
+        Text("Guess the Flag")
+          .font(.largeTitle.weight(.bold))
+          .foregroundColor(.white)
         
-        Text("Content")
-          .foregroundStyle(.secondary)
-          .padding(50)
-          .background(.ultraThinMaterial)
-          .cornerRadius(10.0)
-        
-        Spacer()
-        
-        Button(action: onButtonPress, label: {
-          Label("Take Photo", systemImage: "camera.shutter.button")
-        })
-          .alert("Photo taken!", isPresented: $showingAlert) {
-            Button("Delete", role: .destructive) {
-              print("Delete button pressed!")
-            }
-            Button("Second button") {
-              print("Second button pressed!")
-            }
-            Button("Cancel", role: .cancel) {
-              print("Cancel button pressed!")
-            }
-          } message: {
-            Text("This simulates a photo being taken.")
+        VStack(spacing: 15) {
+          
+          VStack {
+            Text("Tap the flag of")
+              .font(.subheadline.weight(.heavy))
+              .foregroundStyle(.secondary)
+            
+            Text(countries[correctAnswer])
+              .font(.largeTitle.weight(.semibold))
           }
-          .buttonStyle(.borderedProminent)
-          .tint(.secondary)
-          .foregroundColor(.primary)
+          .foregroundColor(.white)
+          
+          ForEach(0..<3) { number in
+            Button {
+              flagTapped(number)
+            } label: {
+              Image(countries[number])
+                .renderingMode(.original)
+                .clipShape(Capsule())
+                .shadow(radius: 5)
+            }
+          }
+        }
+        .frame(maxWidth: .infinity)
+        .padding(.vertical, 20)
+        .background(.regularMaterial)
+        .clipShape(RoundedRectangle(cornerRadius: 20))
         
-        Spacer()
+        Text("Score: \(score)")
+          .foregroundColor(.white)
+          .font(.title.bold())
       }
+      .padding()
     }
     .ignoresSafeArea()
+    .alert(scoreTitle, isPresented: $showingScore) {
+      if attempts <= totalQuestions {
+        Button("Continue", action: askQuestion)
+      } else {
+        Button("Start Over", action: reset)
+      }
+    } message: {
+      if attempts <= totalQuestions {
+        Text("Your score is \(score)")
+      } else {
+        Text("Here are your final results. You got \(score) correct, out of \(attempts) questions.")
+      }
+    }
   }
   
-  func onButtonPress() {
-    showingAlert = true
+  func flagTapped(_ number: Int) {
+    attempts += 1
+    if number == correctAnswer {
+      scoreTitle = "Correct"
+      score += 1
+    } else {
+      scoreTitle = "Wrong! That's the flag of \(countries[number])"
+    }
+    
+    showingScore = true
+  }
+  
+  func askQuestion() {
+    countries.shuffle()
+    correctAnswer = Int.random(in: 0...2)
+  }
+  
+  func reset() {
+    scoreTitle = ""
+    score = 0
+    attempts = 0
   }
 }
 
